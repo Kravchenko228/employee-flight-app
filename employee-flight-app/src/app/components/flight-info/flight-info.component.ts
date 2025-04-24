b76183f88a93ca49dbd3328cd05873d34484de3e
Kravchenko Georgy <88142456+Kravchenko228@users.noreply.github.com>
2025-04-24 14:52:28 +0000

    duration and edited timer

--- employee-flight-app/src/app/components/flight-info/flight-info.component.ts
+++ employee-flight-app/src/app/components/flight-info/flight-info.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Flight } from '../../models/flight.model';
+import { DurationPipe } from '../../pipes/duration.pipe';
 
 @Component({
   selector: 'app-flight-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DurationPipe],
 
   templateUrl: './flight-info.component.html',
   styleUrl: './flight-info.component.css'
